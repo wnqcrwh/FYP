c82fb334d43b64aa3745e06af9a45b0ac95a380c
wnqcrwh <mrcwnyyxa@gmail.com>
2025-04-11 05:20:47 +0800

    audio_model

--- train.py
+++ train.py
@@ -11,16 +11,15 @@ import torch.nn.functional as F
 class MultiModalModel(nn.Module):
     def __init__(self, num_classes=(7,3)):
         super().__init__()
+        self.face_model = mobilefacenet.MobileFaceNet() #(B, T, 128)
         self.video_model = mobilefacenet.MobileFaceNet() #(B, T, 128)
         self.audio_model = audio_model.AudioCNN() #(B, T, 128)
-        self.fc = nn.Linear(256 + 512, num_classes)  # Combine video and audio features
 
-    def forward(self, video, audio):
+    def forward(self,original_frames, video, audio):
+
         video_features = self.video_model(video)
         audio_features = self.audio_model(audio)
-        combined_features = torch.cat((video_features, audio_features), dim=1)
-        output = self.fc(combined_features)
-        return output
+        
 
 
 
